8ac2604437ca3c69cfa798cf76581a76a6eaae5c
Noah Jutz <noahjutz@protonmail.com>
2020-09-23 14:18:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testmigratecompose/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testmigratecompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.test
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        test
+    }
+}
